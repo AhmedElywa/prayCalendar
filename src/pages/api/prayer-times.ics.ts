9a6e21e5541f1387658ddc57fa3ef163c8e6af00
Ahmed Elywa <ahmed.elywa@icloud.com>
2024-01-02 13:38:02 +0200

    Add duration and alarm selection to prayer times
    New options to set alarm and duration for prayer times have been introduced. This enhancement provides more granular control for users over the prayer time notifications. It also enriches the generated prayer times link with additional parameters reflecting these settings.

--- src/pages/api/prayer-times.ics.ts
+++ src/pages/api/prayer-times.ics.ts
@@ -1,37 +1,39 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { getPrayerTimes } from 'prayerTimes'
-import ical, { ICalAlarmType } from 'ical-generator'
-import moment from 'moment/moment'
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getPrayerTimes } from 'prayerTimes';
+import ical, { ICalAlarmType } from 'ical-generator';
+import moment from 'moment/moment';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	if (req.method !== 'GET') {
-		res.status(405).send({message: 'Only GET requests allowed'});
-		return;
-	} else {
-		const { address, method } = req.query;
-		const prayerTimes = await getPrayerTimes(address as string, method ? +method : 5);
-		const allowedEvents = ['Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha'];
-		const calendar = ical({
-			name: 'Prayer Times',
-			timezone: prayerTimes.data[0].meta.timezone,
-		});
+  if (req.method !== 'GET') {
+    res.status(405).send({ message: 'Only GET requests allowed' });
+    return;
+  } else {
+    const { address, method, alarm, duration } = req.query;
+    const prayerTimes = await getPrayerTimes(address as string, method ? +method : 5);
+    const allowedEvents = ['Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha'];
+    const calendar = ical({
+      name: 'Prayer Times',
+      timezone: prayerTimes.data[0].meta.timezone,
+    });
 
-		for (const day of prayerTimes.data) {
-			for (const [name, time] of Object.entries(day.timings)) {
-				if (!allowedEvents.includes(name)) continue;
-				const startDate = moment(`${day.date.gregorian.date} ${time}`, 'DD-MM-YYYY HH:mm').toDate();
-				const event = calendar.createEvent({
-					start: startDate,
-					end: moment(startDate).add(25, 'minute').toDate(),
-					summary: name,
-				});
-				event.createAlarm({
-					type: ICalAlarmType.audio,
-					triggerBefore: 15 * 60
-				})
-			}
-		}
-		res.setHeader('Content-Type', 'text/calendar');
-		res.send(calendar.toString());
-	}
+    for (const day of prayerTimes.data) {
+      for (const [name, time] of Object.entries(day.timings)) {
+        if (!allowedEvents.includes(name)) continue;
+        const startDate = moment(`${day.date.gregorian.date} ${time}`, 'DD-MM-YYYY HH:mm').toDate();
+        const event = calendar.createEvent({
+          start: startDate,
+          end: moment(startDate)
+            .add(duration ? +duration : 25, 'minute')
+            .toDate(),
+          summary: name,
+        });
+        event.createAlarm({
+          type: ICalAlarmType.audio,
+          triggerBefore: (alarm ? +alarm : 15) * 60,
+        });
+      }
+    }
+    res.setHeader('Content-Type', 'text/calendar');
+    res.send(calendar.toString());
+  }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,11 @@ import defaultMethod from 'Components/defaultMethod';
 const Index: React.FC = () => {
   const [address, setAddress] = React.useState('');
   const [method, setMethod] = React.useState('5');
-  const link = `https://pray.ahmedelywa.com/api/prayer-times.ics?address=${encodeURIComponent(address)}&method=${method}`;
+  const [alarm, setAlarm] = React.useState('5');
+  const [duration, setDuration] = React.useState(25);
+  const link = `https://pray.ahmedelywa.com/api/prayer-times.ics?address=${encodeURIComponent(
+    address,
+  )}&method=${method}&alarm=${alarm}&duration=${duration}`;
   return (
     <div className="w-full text-gray-700 flex justify-center py-8 px-4 flex-col max-w-3xl mx-auto space-y-8">
       <h1 className="font-bold text-2xl">Generate Pray Calendar Subscribe link</h1>
@@ -21,10 +25,45 @@ const Index: React.FC = () => {
       </label>
       <label className="flex flex-col font-medium text-gray-700 space-2-8">
         Method
-        <select defaultValue={method} onChange={(event) => setMethod(event.target.value)} className="border p-2 rounded-md border-sky-400">
-          {defaultMethod.map((method) => (<option key={method.value} value={method.value}>{method.label}</option>))}
+        <select
+          defaultValue={method}
+          onChange={(event) => setMethod(event.target.value)}
+          className="border p-2 rounded-md border-sky-400"
+        >
+          {defaultMethod.map((method) => (
+            <option key={method.value} value={method.value}>
+              {method.label}
+            </option>
+          ))}
         </select>
       </label>
+      <div className="grid gap-2 md:grid-cols-2 grid-cols-1">
+        <label className="flex flex-col font-medium text-gray-700 space-2-8">
+          Alarm (minutes before)
+          <select
+            defaultValue={alarm}
+            onChange={(event) => setAlarm(event.target.value)}
+            className="border p-2 rounded-md border-sky-400"
+          >
+            <option value="5">5 minutes</option>
+            <option value="10">10 minutes</option>
+            <option value="15">15 minutes</option>
+            <option value="30">30 minutes</option>
+          </select>
+        </label>
+        <label className="flex flex-col font-medium text-gray-700 space-2-8">
+          Duration (minutes)
+          <input
+            id="duration"
+            name="duration"
+            className="border p-2 rounded-md border-sky-400"
+            value={duration}
+            type="number"
+            onChange={(event) => setDuration(+event.target.value)}
+          />
+        </label>
+      </div>
+
       <a href={link} className="bg-blue-500 text-center text-white font-bold py-2 px-4 rounded-full hover:bg-blue-600">
         Subscribe to Calendar
       </a>
