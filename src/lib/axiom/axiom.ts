c8ff42a09f177ef7deaf6855a523be3a087ccdc6
Ahmed Elywa <AhmedElywordy@gmail.com>
2026-01-29 22:31:44 +0100

    Switch Axiom to US region endpoint

--- src/lib/axiom/axiom.ts
+++ src/lib/axiom/axiom.ts
@@ -2,7 +2,7 @@ import { Axiom } from '@axiomhq/js';
 
 const axiomClient = new Axiom({
   token: process.env.AXIOM_TOKEN!,
-  url: process.env.AXIOM_URL || 'https://api.eu.axiom.co',
+  url: process.env.AXIOM_URL || 'https://api.axiom.co',
 });
 
 export default axiomClient;
