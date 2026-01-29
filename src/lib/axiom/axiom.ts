52111c9c0e5c39e682f1b6ee46bdbd5d6a326198
Ahmed Elywa <AhmedElywordy@gmail.com>
2026-01-29 22:24:09 +0100

    Use EU Axiom API endpoint for dataset in eu-central-1

--- src/lib/axiom/axiom.ts
+++ src/lib/axiom/axiom.ts
@@ -2,6 +2,7 @@ import { Axiom } from '@axiomhq/js';
 
 const axiomClient = new Axiom({
   token: process.env.AXIOM_TOKEN!,
+  url: process.env.AXIOM_URL || 'https://api.eu.axiom.co',
 });
 
 export default axiomClient;
